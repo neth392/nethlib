e7337ec03139e07dbe1897f02b0213f0f68a6c8f
neth392 <cneth97@gmail.com>
2024-07-22 08:28:18 -0400

    cleanup

--- playground/general_testing.gd
+++ playground/general_testing.gd
@@ -2,11 +2,3 @@
 extends Node
 
 @export var id: String
-
-func _ready() -> void:
-	var test: Test2 = Test2.new()
-
-	for method: Dictionary in test.get_script().get_script_method_list():
-		print(" ")
-		print(method)
-		print(" ")
