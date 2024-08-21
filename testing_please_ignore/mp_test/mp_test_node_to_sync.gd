1e52fc59eabfd5154445db4f4fdb43264de7fe10
neth392 <cneth97@gmail.com>
2024-08-21 14:48:45 -0400

    messing around with multiplayer

---
+++ testing_please_ignore/mp_test/mp_test_node_to_sync.gd
@@ -0,0 +1,19 @@
+class_name MpTestNodeToSync extends VBoxContainer
+
+@export var test_sync_string: String:
+	set(value):
+		test_sync_string = value
+		if string_label != null:
+			string_label.text = value
+
+@export var test_dictionary: Dictionary
+
+@export var test_array: Array[int]
+
+@onready var string_label: Label = $StringLabel
+
+func _ready() -> void:
+	string_label.text = test_sync_string
+
+func _to_string() -> String:
+	return ObjectUtil.to_string_helper("MpTestNodeToSync", self)
--- testing_please_ignore/mp_test/multiplayer_test.gd
+++ testing_please_ignore/mp_test/multiplayer_test.gd
@@ -3,17 +3,26 @@ class_name MultiplayerTest extends Node
 const IP_ADDRESS: String = "localhost"
 const PORT: int = 25565
 
+@export var host_label: Label
 @export var server_ui: CanvasLayer
 @export var join_button: Button
 @export var host_button: Button
+@export var spawn_button: Button
+@export var scene_to_spawn: PackedScene
+@export var sync_nodes: Node
+@export var add_to_string_button: Button
+
+@onready var synchronizer: MultiplayerSynchronizer = $MultiplayerSynchronizer
 
 func _ready() -> void:
 	host_button.pressed.connect(host)
 	join_button.pressed.connect(join)
+	spawn_button.pressed.connect(_spawn_pressed)
 
 
 func host() -> void:
 	print("host()")
+	host_label.text = "HOST"
 	join_button.queue_free()
 	host_button.queue_free()
 	var server_peer: MultiplayerPeer = ENetMultiplayerPeer.new()
@@ -25,6 +34,7 @@ func host() -> void:
 
 func join() -> void:
 	print("join()")
+	host_label.text = "CLIENT"
 	join_button.queue_free()
 	host_button.queue_free()
 	var client_peer: MultiplayerPeer = ENetMultiplayerPeer.new()
@@ -39,5 +49,28 @@ func join() -> void:
 func _on_peer_connected(id: int) -> void:
 	print("Peer connected: %s" % id)
 
+
 func _on_peer_disconnected(id: int) -> void:
 	print("Peer disconnected: %s" % id)
+
+
+func _spawn_pressed() -> void:
+	if multiplayer.multiplayer_peer.get_connection_status() != MultiplayerPeer.CONNECTION_CONNECTED:
+		print("_spawn_pressed: NOT CONNECTED")
+		return
+	if !is_multiplayer_authority():
+		print("_spawn_pressed: NOT AUTHORITY")
+		return
+	_kill_spawn_button.rpc()
+	print("_spawn_pressed: IS AUTHORITY")
+	var node: MpTestNodeToSync = scene_to_spawn.instantiate() as MpTestNodeToSync
+	sync_nodes.add_child(node, true)
+	
+	synchronizer.root_path = node.get_path()
+	synchronizer.replication_config.add_property(":test_sync_string")
+	node.test_sync_string = "Changed!"
+
+
+@rpc("authority", "call_local")
+func _kill_spawn_button() -> void:
+	spawn_button.queue_free()
