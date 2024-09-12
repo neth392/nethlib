771f4ce23c766eefde525365b9fd2e22d05e18a7
neth392 <cneth97@gmail.com>
2024-09-12 14:51:14 -0400

    dev

--- testing_please_ignore/general_testing.gd
+++ testing_please_ignore/general_testing.gd
@@ -1,23 +1,5 @@
 class_name GeneralTesting extends Node
 
-
-var test_dictionary: Dictionary[String, int] = {
-	"one": 1,
-	"two": 2,
-	"three": 3,
-}
-
-
 func _ready() -> void:
-	var obj: TestParent = load("res://testing_please_ignore/test_parent_res.tres")
-	
-	var json: String = JSON.stringify(JSON.from_native(obj, true, true))
-	print(JSON.from_native(obj))
-	print(obj)
-	var parsed: Variant = JSON.to_native(JSON.parse_string(json), true, true)
-	print(parsed)
-	print(typeof(parsed))
-	# Trying to assign value of type 'String' to a variable of type 'Vector2'.
-	obj = null
-	obj = parsed
-	print(obj)
+	var type: Variant.Type = TYPE_STRING
+	typeof()
---
+++ testing_please_ignore/my_class.gd
@@ -0,0 +1,3 @@
+class_name MyClass extends Resource
+	
+@export var serialize_this: String = "test"
